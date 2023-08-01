2d955cb5fad4a9fee389a2722b37c3cb2f38ca90
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-08-01 17:42:51 +0530

    feat: passing props compatability

--- input.jsx
+++ input.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
-import { toJson } from 'react-json-parser';
+import React from "react";
+import escapeHtml from "escape-html";
 
-const JSXCode = <div>Hello, world!</div>;
+function renderJSXToHTML(jsx) {
+  if (typeof jsx === "string" || typeof jsx === "number") {
+    return escapeHtml(jsx);
+  } else if (typeof jsx === null || typeof jsx === "boolean") {
+    return "";
+  } else if (Array.isArray(jsx)) {
+    return jsx.map((code) => renderJSXToHTML(code)).join("");
+  } else if (typeof jsx === "object") {
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      let html = "<" + jsx.type;
 
-const jsonOutput = toJson(JSXCode);
-console.log(jsonOutput);
+      for (const propName in jsx.props) {
+        if (propName !== "children") {
+          html = html + " ";
+          html = html + propName;
+          html = html + "=";
+          html = html + escapeHtml(jsx.props[propName]);
+        }
+      }
+      html = html + " >";
+      html = html + renderJSXToHTML(jsx.props.children ?? "");
+      html += "</" + jsx.type + ">";
+      return html;
+    } else throw new Error("cannot render an object");
+  }
+}
+
+function JSX({ postContent, author }) {
+  return (
+    <html>
+      <head>
+        <title>My blog</title>
+      </head>
+      <body>
+        <nav>
+          <a href="/">Home</a>
+          <hr />
+        </nav>
+        <article>{escapeHtml(postContent)}</article>
+        <footer>
+          <hr />
+          <p>
+            <i>
+              (c) {escapeHtml(author)}, {new Date().getFullYear()}
+            </i>
+          </p>
+        </footer>
+      </body>
+    </html>
+  );
+}
+
+function HTML(...args) {
+  return renderJSXToHTML(JSX(...args));
+}
+
+export default HTML;
--- output.cjs
+++ output.cjs
@@ -1,8 +1,50 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
-var _reactJsonParser = require("react-json-parser");
+var _escapeHtml = _interopRequireDefault(require("escape-html"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-
-console.log(JSXCode);
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function renderJSXToHTML(jsx) {
+  if (typeof jsx === "string" || typeof jsx === "number") {
+    return (0, _escapeHtml["default"])(jsx);
+  } else if (typeof jsx === null || typeof jsx === "boolean") {
+    return "";
+  } else if (Array.isArray(jsx)) {
+    return jsx.map(function (code) {
+      return renderJSXToHTML(code);
+    }).join("");
+  } else if (_typeof(jsx) === "object") {
+    if (jsx.$$typeof === Symbol["for"]("react.element")) {
+      var _jsx$props$children;
+      var html = "<" + jsx.type;
+      for (var propName in jsx.props) {
+        if (propName !== "children") {
+          html = html + " ";
+          html = html + propName;
+          html = html + "=";
+          html = html + (0, _escapeHtml["default"])(jsx.props[propName]);
+        }
+      }
+      html = html + " >";
+      html = html + renderJSXToHTML((_jsx$props$children = jsx.props.children) !== null && _jsx$props$children !== void 0 ? _jsx$props$children : "");
+      html += "</" + jsx.type + ">";
+      return html;
+    } else throw new Error("cannot render an object");
+  }
+}
+function JSX(_ref) {
+  var postContent = _ref.postContent,
+    author = _ref.author;
+  return /*#__PURE__*/_react["default"].createElement("html", null, /*#__PURE__*/_react["default"].createElement("head", null, /*#__PURE__*/_react["default"].createElement("title", null, "My blog")), /*#__PURE__*/_react["default"].createElement("body", null, /*#__PURE__*/_react["default"].createElement("nav", null, /*#__PURE__*/_react["default"].createElement("a", {
+    href: "/"
+  }, "Home"), /*#__PURE__*/_react["default"].createElement("hr", null)), /*#__PURE__*/_react["default"].createElement("article", null, (0, _escapeHtml["default"])(postContent)), /*#__PURE__*/_react["default"].createElement("footer", null, /*#__PURE__*/_react["default"].createElement("hr", null), /*#__PURE__*/_react["default"].createElement("p", null, /*#__PURE__*/_react["default"].createElement("i", null, "(c) ", (0, _escapeHtml["default"])(author), ", ", new Date().getFullYear())))));
+}
+function HTML() {
+  return renderJSXToHTML(JSX.apply(void 0, arguments));
+}
+var _default = HTML;
+exports["default"] = _default;
